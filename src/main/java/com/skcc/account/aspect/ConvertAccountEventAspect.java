f11e487c006d3f112d91f9fbd46e0635cc27aaee
noisonnoiton <noisonnoiton@gmail.com>
2021-06-08 13:16:23 +0900

    change txid generate logic

--- src/main/java/com/skcc/account/aspect/ConvertAccountEventAspect.java
+++ src/main/java/com/skcc/account/aspect/ConvertAccountEventAspect.java
@@ -1,5 +1,9 @@
 package com.skcc.account.aspect;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 import com.skcc.account.event.message.AccountEventType;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -27,7 +31,11 @@ public class ConvertAccountEventAspect {
 		//subsribe에 의한 호출시 txId != null
 		if(txId == null) {
 			ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-			txId = attr.getRequest().getHeader("X-TXID");
+			
+			// zuul prefilter 제거하여 수동 생성
+			// txId = attr.getRequest().getHeader("X-TXID");
+			UUID uuid = UUID.randomUUID();
+			txId = String.format("%s-%s", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()), uuid.toString());
 		}
 		
 		return pjp.proceed(new Object[] {txId, id, accountEventType});
